85ef82f40365e4bf554fabe497edf0592cd44f3a
vlad00550 <vlad0055099@gmail.com>
2022-12-04 22:45:14 +0700

    added work with negative numbers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-double my_pow(double num, unsigned int pow) {
-	double res = 1;
-	for (unsigned int i = 0;i < pow;i++) {
-		res *= num;
-	}
-	return res;
+double my_pow(double num, int pow) {
+    double res = 1;
+    char mines = pow < 0;
+    pow = abs(pow);
+    for (unsigned int i = 0;i < pow;i++) {
+        res *= num;
+    }
+    return (mines) ? (1.0/result) : result;
 }
